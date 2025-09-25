340390531be734c00bbbf3f5108f6891513aa4df
Bernitafebri <bernitafebri@gmail.com>
2025-09-25 15:35:33 +0700

    basic

---
+++ basics/basics.py
@@ -0,0 +1,44 @@
+print("--- Learn the Basics ---")
+print()
+print("#Hello World")
+print("Hello World!!!")
+print()  # enter / baris kosong
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+print()  # enter / baris kosong
+
+print("#Variables and Types")
+print("A. Numbers")
+myint = 7
+print(myint)
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+print()  # enter / baris kosong
+
+print("B. Strings")
+mystring = 'hello'
+print(mystring)
+mystring2 = "Don't worry about apostrophes"
+print(mystring2)
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+a, b = 3, 4
+print(a, b)
+print()  # enter / baris kosong
+
+
+
+print()  # enter / baris kosong
